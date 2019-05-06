56fd7fc8e95f13edb6e891415338cc27b8050550
CJHydraGenZ <cahyasaga321@gmail.com>
2019-05-06 12:46:16 +0800

    mengatasi bug

--- App/model/mahasiswa_model.php
+++ App/model/mahasiswa_model.php
@@ -73,7 +73,7 @@ class mahasiswa_model
     }
     public function getMahasiswaByID($id)
     {
-        $this->db->query('SELECT * FROM' . $this->table . 'WHERE id=:id');
+        $this->db->query('SELECT * FROM mahasiswa WHERE id =:id');
         $this->db->bind('id', $id);
         return $this->db->single();
     }
