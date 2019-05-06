ade5334b27277088f8422534d5136bb2d14a52ea
CJHydraGenZ <cahyasaga321@gmail.com>
2019-05-06 10:52:44 +0800

    tambah asset

--- App/view/about/index.php
+++ App/view/about/index.php
@@ -1,2 +1,5 @@
-<h1>About me</h1>
-<p>hllo nama saya <?= $data['nama'];   ?> , umur saya <?= $data['umur'];   ?> tahun,saya seorang <?= $data['perkerjaan'];   ?> </p>
+<div class="container">
+    <h1>About me</h1>
+    <img src="<?= BASEURL;   ?>/img/mashu_kyrielight__shielder__render___fate_by_galangcp-dbvb64w.png" alt="Cahya" width="200" class="rounded shadow">
+    <p>hllo nama saya <?= $data['nama'];   ?> , umur saya <?= $data['umur'];   ?> tahun,saya seorang <?= $data['perkerjaan'];   ?> </p>
+</div>
